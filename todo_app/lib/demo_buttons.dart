d3ce83f01a0c87090a0b50e9c41e278441fe0727
Ann Lee <ann@meloq.se>
2025-02-06 11:07:58 +0100

    debug

--- todo_app/lib/demo_buttons.dart
+++ todo_app/lib/demo_buttons.dart
@@ -14,7 +14,6 @@ class _DemoButtonsState extends State<DemoButtons> {
 
   @override
   Widget build(BuildContext context) {
-    print('DemoButtons BUILD called');
     return Column(
       mainAxisSize: MainAxisSize.min,
       children: [
